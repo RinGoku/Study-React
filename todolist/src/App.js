bd5685c2bda6ecfe02d96ed52703ccdddd0d9e62
yuyu80105 <yuyu80105@gmail.com>
2019-01-24 07:45:11 -0500

    todolist complete! except design...

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,37 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class TodoInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoInput: ''
-    }
-  }
+class TodoInput extends Component {  
   render() {
     return (
       <section>
-        <input type="text" value={this.state.todoInput} onChange={}/>
-        <button>追加</button>
+        <input type="text" value={this.props.todoInput} 
+        onChange={this.props.updateInput}/>
+        <button onClick={this.props.clickItem}>追加</button>
       </section>
     )
   }
 }
 
-class TodoItem extends Component {
-  render() {
-    return (
-      <li>aaaa</li>
-    )
-  }  
-}
-
 class TodoList extends Component {
   render() {
+    const propTodoList = this.props.todoList;
+    const todoList = propTodoList.map((todo, index) => {
+      return (<li key={index}>
+              {todo}
+              <button onClick={() => this.props.removeTodo(index)}>×</button>
+              </li>);
+    });
     return (
       <section>
         <ol>
-          <TodoItem />
+          {todoList}
         </ol>
       </section>
     )
@@ -42,18 +36,42 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      todoInput: '',
       todoList: []
     }
   }
+  addTodo() {
+    const newTodoList = this.state.todoList.slice();
+    newTodoList.push(this.state.todoInput);
+    this.setState({
+      todoList: newTodoList
+    });
+  }
+  removeTodo(i) {
+    const newTodoList = this.state.todoList.slice();
+    newTodoList.splice(i, 1); 
+    this.setState({
+      todoList: newTodoList
+    });
+  }
+  updateInput(e) {
+    this.setState({todoInput: e.target.value});
+  }
   render() {
     return (
-
       <div className="App">
             <section>
-              <TodoInput />
+              <TodoInput 
+               clickItem={() => this.addTodo()}
+               todoInput={this.state.todoInput}
+               updateInput={e => this.updateInput(e)}
+              />
             </section>
             <section>
-              <TodoList />
+              <TodoList 
+               todoList={this.state.todoList}
+               removeTodo={(i) => this.removeTodo(i)}
+              />
             </section>
       </div>
     );
