b63383d1d79cc16a8bc30c24263cf138abb75e91
yuyu80105 <yuyu80105@gmail.com>
2019-01-26 06:16:59 -0500

    create dislay color page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,66 @@
 import * as React from 'react';
 import './App.css';
 import './App.scss';
-
-class App extends React.Component {
+interface ColorUnitProps {
+  thisColor: string;
+}
+class ColorUnit extends React.Component<ColorUnitProps, {}> {
   public render() {
     return (
+      <section className="color-unit" style={{backgroundColor: this.props.thisColor}}>
+      </section>
+    );
+  }
+}
+
+class App extends React.Component {
+  hexMap = {
+    0: '0',
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: 'a',
+    11: 'b',
+    12: 'c',
+    13: 'd',
+    14: 'e',
+    15: 'f'
+  };
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      colors: this.createColors()
+    }
+  }
+  createColors(): string[] {
+    const colors = [];
+    for(let i = 1; i < 256; i+=3) {
+      for(let j = 1; j < 256; j+=3) {
+        for(let k = 1; k < 256; k+=3) {
+          colors.push(`rgb(${i},${j},${k})`);
+        }
+      }
+    }
+    return colors;
+  }
+  
+  public render() {
+   const colors = (this.state as any).colors.map((thisColor: any) => {
+    return (
+      <ColorUnit key={thisColor} thisColor={thisColor}/>
+    );
+   });
+    return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <section className="colors-wrapper">
+          {colors}
+        </section>
       </div>
     );
   }
